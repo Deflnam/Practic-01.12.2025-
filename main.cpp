38931f7bf9636a6e7d6f2c036eebd6d59eb9bd94
Deflnam <burukov01@bk.ru>
2025-12-08 17:22:38 +0300

    Colpete(+-)

--- main.cpp
+++ main.cpp
@@ -32,13 +32,18 @@ namespace top {
     p_t next(p_t p) const override;
     p_t o;
   };
-  void extend(p_t** ps, size_t s, p_t p);
+
+  void extend(p_t** ps, size_t& s, p_t p); 
   void make_f(IDraw** f, size_t k);
   size_t getPoints(IDraw* f, p_t** ps, size_t& s);
-  Frame_t buildFrame(const p_t* ps, size_t s); // Ищем мин и макс для х и у
-  char* buildCanvas(Frame_t fr); // на основе фрейма считаем макс - мин + 1
-  void paintCanvas(char* cnv, Frame_t fr, const p_t* ps , size_t k, char f); // координаты перевести в коорд канваса ужас
-  void printCanvas(char* cnv, Frame_t fr);  // ТОЛЬКО ПОПРОБУЙ ВЫВЕСТИ ЛИШНИЙ ПРОБЕЛ!!!!
+  Frame_t buildFrame(const p_t* ps, size_t s);
+  char* buildCanvas(Frame_t fr, char);
+  void paintCanvas(char* cnv, Frame_t fr, const p_t* ps, size_t k, char f);
+  void printCanvas(char* cnv, Frame_t fr);
+
+
+  size_t rows(Frame_t fr);
+  size_t cols(Frame_t fr);
 
   struct VLine : IDraw {
     VLine(int x, int y, int len);
@@ -64,7 +69,6 @@ namespace top {
     p_t start;
     int len;
   };
-
 }
 
 
@@ -73,7 +77,7 @@ int main()
 {
   top::IDraw* f[3] = {};
   top::p_t* p = nullptr;
-  size_t s = 0;
+  size_t s = 0; 
   char* cnv = nullptr;
   int statusCode = 0;
   try {
@@ -82,8 +86,8 @@ int main()
       top::getPoints(f[i], &p, s);
     }
     top::Frame_t fr = top::buildFrame(p, s);
-    cnv = top::buildCanvas(fr);
-    top::paintCanvas(cnv, fr, p, s, 'o');
+    cnv = top::buildCanvas(fr, '.');
+    top::paintCanvas(cnv, fr, p, s, 'o'); 
     top::printCanvas(cnv, fr);
   } catch(...) {
     statusCode = 1;
@@ -105,12 +109,13 @@ top::p_t top::Dot::next(p_t p) const
 
 void top::make_f(IDraw** f, size_t k)
 {
-  f[0] = new Dot(0, 0);
-  f[1] = new Dot(-1, -5);
-  f[2] = new Dot(7, 7);
+  f[0] = new top::Dot(0, 0);
+  f[1] = new top::Dot(1, -5);
+  f[2] = new top::Dot(7, 7);
 }
 
-void top::extend(p_t** ps, size_t s, p_t p)
+
+void top::extend(p_t** ps, size_t& s, p_t p)
 {
   size_t upd_s = s + 1;
   top::p_t* res = new top::p_t[upd_s];
@@ -120,49 +125,90 @@ void top::extend(p_t** ps, size_t s, p_t p)
   res[s] = p;
   delete[] *ps;
   *ps = res;
+  s = upd_s; 
 }
 
 size_t top::getPoints(IDraw* f, p_t** ps, size_t& s)
 {
   p_t p = f->begin();
   extend(ps, s, p);
-  size_t delta = 1;
-  while (f->next(p) != f->begin()) {
-    p = f->next(p);
-    extend(ps, s+delta, p);
-    delta++;
+  p_t current = p;
+  p_t next_p = f->next(current);
+  
+  size_t count = 1;
+  while (next_p != f->begin()) {
+    extend(ps, s, next_p);
+    current = next_p;
+    next_p = f->next(current);
+    count++;
+    if (count > 1000) break; 
   }
-   return delta;
+  
+  return count;
 }
 
 top::Frame_t top::buildFrame(const p_t* ps, size_t s)
 {
   if (!s) {
-    throw std::logic_error("");
+    throw std::logic_error("No points");
   }
   int minx = ps[0].x, maxx = ps[0].x;
   int miny = ps[0].y, maxy = ps[0].y;
   for (size_t i = 1; i < s; ++i) {
-    minx = std::min(minx, ps[i].x);
-    maxx = std::max(maxx, ps[i].x);
-    miny = std::min(miny, ps[i].y);
-    maxy = std::max(maxy, ps[i].y);
+    if (ps[i].x < minx) minx = ps[i].x;
+    if (ps[i].x > maxx) maxx = ps[i].x;
+    if (ps[i].y < miny) miny = ps[i].y;
+    if (ps[i].y > maxy) maxy = ps[i].y;
   }
   p_t aa {minx, miny};
-  p_t bb { maxx, maxy};
+  p_t bb {maxx, maxy};
   return {aa, bb};
 }
 
-char* top::buildCanvas(Frame_t fr)
+size_t top::rows(Frame_t fr)
 {
+  return fr.rightTop.y - fr.leftBott.y + 1;
 }
+
+size_t top::cols(Frame_t fr)
+{
+  return fr.rightTop.x - fr.leftBott.x + 1;
+}
+
+char* top::buildCanvas(Frame_t fr, char fill)
+{
+  size_t r = top::rows(fr);
+  size_t c = top::cols(fr);
+  char* newcanws = new char[r * c];
+  for (size_t i = 0; i < r * c; ++i) {
+    newcanws[i] = fill;
+  }
+  return newcanws;
+}
+
 
 void top::paintCanvas(char* cnv, Frame_t fr, const p_t* ps, size_t k, char f)
 {
+  size_t c = top::cols(fr);
+  for (size_t i = 0; i < k; ++i) {
+    int dx = ps[i].x - fr.leftBott.x;
+    int dy = fr.rightTop.y - ps[i].y;
+    if (dx >= 0 && dx < (int)c && dy >= 0 && dy < (int)top::rows(fr)) {
+      cnv[dy * c + dx] = f;
+    }
+  }
 }
 
 void top::printCanvas(char* cnv, Frame_t fr)
 {
+  size_t r = top::rows(fr);
+  size_t c = top::cols(fr);
+  for (size_t i = 0; i < r; ++i) {
+    for (size_t j = 0; j < c; ++j) {
+      std::cout << cnv[i * c + j];
+    }
+    std::cout << '\n';
+  }
 }
 
 top::p_t top::Dot::begin() const
@@ -211,9 +257,9 @@ top::p_t top::HLine::next(p_t p) const
     return start;
   }
   if (length > 0) {
-    return p_t{p.x + 1, start.y };
+    return p_t{p.x + 1, start.y};
   }
-  return p_t{p.x - 1, start.y };
+  return p_t{p.x - 1, start.y};
 }
 
 top::Square::Square(int x, int y, int l):
@@ -226,9 +272,12 @@ top::Square::Square(int x, int y, int l):
   }
 }
 
-top::Square::Square(p_t p, int l)
+top::Square::Square(p_t p, int l) : start{p.x, p.y}, len(l)
 {
-  Square(p.x, p.y, l);
+  
+  if (len <= 0) {
+    throw std::invalid_argument("lenght can not be  <= 0");
+  }
 }
 
 top::p_t top::Square::begin() const
@@ -247,4 +296,5 @@ top::p_t top::Square::next(p_t p) const
   } else if (p.y == start.y && p.x > start.x) {
     return {p.x - 1, p.y};
   }
+  return start; 
 }
